25417aa1f2830f3d96128b9303d112a6b0acbb63
cloudicen <448312134@qq.com>
2020-04-01 02:48:12 +0800

    套了个文件夹

--- lower_machine_firmware/uartSwitch/uartSwitch.ino
+++ lower_machine_firmware/uartSwitch/uartSwitch.ino
@@ -1,22 +1,27 @@
+//注意，输入端检测到低电平时代表触发，输出端触发时输出高电平，因此控制用的开关应跨接在输入引脚和地之间，选用的继电器模块应该是高电平触发的
 //all input pin triggers while LOW!!!! and output HIGH!!!!
 
-//#define SELF_RECOVERY  //uncomment this if you are using a self recovery switch
+//#define SELF_RECOVERY  //uncomment this if you are using a self recovery switch 如果你使用的是自回弹的开关，把这行代码取消注释
 
-#define PIN_COUNT 4     //define the quantity of pins that used for IO
-#define BAUD 9600 //baud rate
+#define PIN_COUNT 4     //define the quantity of pins that used for IO 定义开关数量，数字1-8
+#define BAUD 9600 //baud rate 串口波特率，一般不用改
 /*
  * now you need to define the pin that used for input and out put
  * make sure the quantity of input pins and output pins are same, and also qual to 'pinCount' definded above!
  * this firmware support up to 8-way switch (8 pins for input and 8 pins for output)
  * because arduino nano only have 19 pins for io, and uart port need 2 pins to work. Also, states of 8 pins can be easily presented using 8bits(char or uint8_t) and esay for transmitting.
  */
+//定义用到的io口，inputPinList定义用于输入的引脚，outputPinList定义用于输出的引脚，引脚之间是一一对应关系，即数组中相同位置的输入引脚控制相同位置的输出引脚，数量必须与之前定义的开关数量相同
+//以下为arduino uno/nano/pro mini/mini的预定义引脚
+
 //uint8_t inputPinList[] = {2, 3, 4, 5, 6, 7, 8, 9};    //maximum pin using in arduino uno/nano/pro mini/mini
 //uint8_t outputPinList[] = {10,11,12,13,14,15,16,17};  //maximum pin using in arduino uno/nano/pro mini/mini
 
+//以下为esp8266 nodemcu的预定义引脚
 const uint8_t inputPinList[] = {0, 4, 5,10}; //maximum pin using in esp8266-12e nodemcu
 const uint8_t outputPinList[] = {15, 13, 12, 14};//maximum pin using in esp8266-12e nodemcu
 
-
+//========以下代码无需修改=======//
 //========code below no need to change======//
 
 //commands defination
